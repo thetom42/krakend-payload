41edcdd05a4831ed6dcad2c94f8ddad022b1321c
Thomas Hessel <thetom42@gmail.com>
2021-08-29 18:02:52 +0000

    Use solution from blog

--- payload.go
+++ payload.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"html"
 	"net/http"
+
+	"io/ioutil"
+	"time"
 )
+
+const outputHeaderName = "X-Friend-User"
 
 // HandlerRegisterer is the symbol the plugin loader will try to load. It must implement the Registerer interface
 var HandlerRegisterer = registerer("krakend-payload")
@@ -20,18 +24,59 @@ func (r registerer) RegisterHandlers(f func(
 	f(string(r), r.registerHandlers)
 }
 
-func (r registerer) registerHandlers(ctx context.Context, extra map[string]interface{}, _ http.Handler) (http.Handler, error) {
-	// check the passed configuration and initialize the plugin
-	name, ok := extra["name"].(string)
+//func (r registerer) registerHandlers(ctx context.Context, extra map[string]interface{}, _ http.Handler) (http.Handler, error) {
+//	// check the passed configuration and initialize the plugin
+//	name, ok := extra["name"].(string)
+//	if !ok {
+//		return nil, errors.New("wrong config")
+//	}
+//	if name != string(r) {
+//		return nil, fmt.Errorf("unknown register %s", name)
+//	}
+//	// return the actual handler wrapping or your custom logic so it can be used as a replacement for the default http handler
+//	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+//		fmt.Fprintf(w, "Hello, %q", html.EscapeString(req.URL.Path))
+//	}), nil
+//}
+
+func (r registerer) registerHandlers(ctx context.Context, extra map[string]interface{}, handler http.Handler) (http.Handler, error) {
+	attachUserID, ok := extra["attachuserid"].(string)
 	if !ok {
-		return nil, errors.New("wrong config")
+		panic(errors.New("incorrect config").Error())
 	}
-	if name != string(r) {
-		return nil, fmt.Errorf("unknown register %s", name)
-	}
-	// return the actual handler wrapping or your custom logic so it can be used as a replacement for the default http handler
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(req.URL.Path))
+
+	client := &http.Client{Timeout: 3 * time.Second}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		rq, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://api.github.com/users/%v", attachUserID), nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		rq.Header.Set("Content-Type", "application/json")
+
+		rs, err := client.Do(rq)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotAcceptable)
+			return
+		}
+		defer rs.Body.Close()
+
+		rsBodyBytes, err := ioutil.ReadAll(rs.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotAcceptable)
+			return
+		}
+
+		r2 := new(http.Request)
+		*r2 = *r
+
+		r2.Header.Set(outputHeaderName, string(rsBodyBytes))
+
+		fmt.Println("krakend-payload handler about to do something")
+
+		handler.ServeHTTP(w, r2)
 	}), nil
 }
 
