c22521781f92a6311ce41729d81c031b999a4867
Thomas Hessel <thetom42@gmail.com>
2021-08-29 20:02:00 +0000

    Fix shell script

--- ge.sh
+++ ge.sh
@@ -1,4 +1,6 @@
 #/bin/bash
+docker stop ge >> /dev/null 2>&1
 docker rm ge >> /dev/null 2>&1
+docker build -t go116 -f Dockerfile .
 docker run -it -v $PWD:/temp --name ge go116 build.sh
-mv krakend-payload.so ../gateway-test/plugins
+mv -f krakend-payload.so ../gateway-test/plugins
