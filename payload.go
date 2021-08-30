3d5ce6a665e37e3a69cf7d41bd69263755641c28
Thomas Hessel <thetom42@gmail.com>
2021-08-30 13:49:43 +0000

    Add client plugin

--- payload.go
+++ payload.go
@@ -4,91 +4,70 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net/http"
-
+	"html"
 	"io/ioutil"
-	"time"
+	"net/http"
 )
-
-const outputHeaderName = "X-Friend-User"
-
-// HandlerRegisterer is the symbol the plugin loader will try to load. It must implement the Registerer interface
-var HandlerRegisterer = registerer("krakend-payload")
 
 type registerer string
 
-func (r registerer) RegisterHandlers(f func(
+// ClientRegisterer is the symbol the plugin loader will try to load. It must implement the RegisterClient interface
+var ClientRegisterer = registerer("krakend-payload-proxy")
+
+func (r registerer) RegisterClients(f func(
 	name string,
-	handler func(context.Context, map[string]interface{}, http.Handler) (http.Handler, error),
+	handler func(context.Context, map[string]interface{}) (http.Handler, error),
 )) {
-	f(string(r), r.registerHandlers)
+	f(string(r), r.registerClients)
 }
 
-//func (r registerer) registerHandlers(ctx context.Context, extra map[string]interface{}, _ http.Handler) (http.Handler, error) {
-//	// check the passed configuration and initialize the plugin
-//	name, ok := extra["name"].(string)
-//	if !ok {
-//		return nil, errors.New("wrong config")
-//	}
-//	if name != string(r) {
-//		return nil, fmt.Errorf("unknown register %s", name)
-//	}
-//	// return the actual handler wrapping or your custom logic so it can be used as a replacement for the default http handler
-//	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-//		fmt.Fprintf(w, "Hello, %q", html.EscapeString(req.URL.Path))
-//	}), nil
-//}
-
-func (r registerer) registerHandlers(ctx context.Context, extra map[string]interface{}, handler http.Handler) (http.Handler, error) {
-	attachUserID, ok := extra["attachuserid"].(string)
+func (r registerer) registerClients(ctx context.Context, extra map[string]interface{}) (h http.Handler,e error) {
+	// check the passed configuration and initialize the plugin
+	name, ok := extra["name"].(string)
 	if !ok {
-		panic(errors.New("incorrect config").Error())
+		return nil, errors.New("wrong config")
 	}
+	if name != string(r) {
+		return nil, fmt.Errorf("unknown register %s", name)
+	}
+	// return the actual handler wrapping or your custom logic so it can be used as a replacement for the default http client
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		fmt.Println("before proxy handler %s",html.EscapeString(req.URL.Path))
+		// fmt.Println(req.URL.Query().Get("appid"))
 
-	client := &http.Client{Timeout: 3 * time.Second}
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		makeOriginalRequest(w,req)
 
-		rq, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://api.github.com/users/%v", attachUserID), nil)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		rq.Header.Set("Content-Type", "application/json")
-
-		rs, err := client.Do(rq)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusNotAcceptable)
-			return
-		}
-		defer rs.Body.Close()
-
-		rsBodyBytes, err := ioutil.ReadAll(rs.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusNotAcceptable)
-			return
-		}
-
-		r2 := new(http.Request)
-		*r2 = *r
-
-		r2.Header.Set(outputHeaderName, string(rsBodyBytes))
-
-		fmt.Println("krakend-payload handler about to do something")
-
-		rBodyBytes, err :=  ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusNotAcceptable)
-			return
-		}
-		fmt.Println(string(rBodyBytes))
-
-		handler.ServeHTTP(w, r2)
+		fmt.Println("after proxy-plugin called")
 	}), nil
 }
 
 func init() {
-	fmt.Println("krakend-payload handler plugin loaded!")
+	fmt.Println("proxy-plugin client plugin loaded!!!")
+}
+
+func makeOriginalRequest(w http.ResponseWriter, req *http.Request) {
+	    client := &http.Client{}
+		// Send an HTTP request and returns an HTTP response object.
+		 resp, err := client.Do(req)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer resp.Body.Close()
+	
+		// headers
+		for name, values := range resp.Header {
+		    w.Header()[name] = values
+		}
+		
+		// status (must come after setting headers and before copying body)
+		w.WriteHeader(resp.StatusCode)
+		
+		body, err := ioutil.ReadAll(resp.Body)		
+		w.Write(body)
+		fmt.Println(string(body))
+
+		fmt.Println("request completed")
 }
 
 func main() {}
